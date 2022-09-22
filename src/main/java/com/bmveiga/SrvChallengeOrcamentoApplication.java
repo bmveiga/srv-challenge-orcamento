747cf21ad6ff0c23af5a56e00d903bfb8259bc42
unknown <barbaramoreira011@gmail.com>
2022-09-22 15:28:50 -0300

    first commit

---
+++ src/main/java/com/bmveiga/SrvChallengeOrcamentoApplication.java
@@ -0,0 +1,13 @@
+package com.bmveiga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrvChallengeOrcamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrvChallengeOrcamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bmveiga/SrvChallengeOrcamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmveiga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrvChallengeOrcamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
